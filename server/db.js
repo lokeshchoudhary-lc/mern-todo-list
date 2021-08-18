267d864ea29c59129facca2094a438ac521bfafe
Lokesh Choudhary <lokeshchoudhary8949@gmail.com>
2021-08-18 15:02:11 +0530

    feat: Add mongoose connection

---
+++ server/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+module.exports = async () => {
+  try {
+    const connectionParams = {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    };
+    await mongoose.connect('mongodb://localhost/todo-app', connectionParams);
+    console.log('Connected to database.');
+  } catch (error) {
+    console.log('Could not connect to database.', error);
+  }
+};
